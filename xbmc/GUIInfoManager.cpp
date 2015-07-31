6082b380570ab73a82c8863615a51de83bcf7531
metaron <andrew@thomasinfletcher.com>
2015-07-31 22:34:28 +0100

    [PVR] Implement: Recording scraper support / UI
    Add recording as source for EpisodeName skinnable value
    Add recording as source for Season/Episode/Year skinnable values
    Confluence skin changes to display recording EpisodeName if available
    pvr:// filename now includes season/episode, year and EpisodeName:
     - UI Sort 'By File' now displays in season/episode order
     - fixes ttvdb scraping of tv shows using pvr:// protocol

--- xbmc/GUIInfoManager.cpp
+++ xbmc/GUIInfoManager.cpp
@@ -4747,6 +4747,8 @@ std::string CGUIInfoManager::GetItemLabel(const CFileItem *item, int info, std::
         if (tag->Year() > 0)
           year = StringUtils::Format("%i", tag->Year());
       }
+      if (item->HasPVRRecordingInfoTag() && item->GetPVRRecordingInfoTag()->m_iYear > 0)
+          year = StringUtils::Format("%i", item->GetPVRRecordingInfoTag()->m_iYear);
       return year;
     }
   case LISTITEM_PREMIERED:
@@ -4970,6 +4972,11 @@ std::string CGUIInfoManager::GetItemLabel(const CFileItem *item, int info, std::
         iSeason = item->GetVideoInfoTag()->m_iSeason;
         iEpisode = item->GetVideoInfoTag()->m_iEpisode;
       }
+      else if (item->HasPVRRecordingInfoTag())
+      {
+        iSeason = item->GetPVRRecordingInfoTag()->m_iSeason;
+        iEpisode = item->GetPVRRecordingInfoTag()->m_iEpisode;
+      }
 
       if (iEpisode >= 0)
       {
@@ -4998,6 +5005,8 @@ std::string CGUIInfoManager::GetItemLabel(const CFileItem *item, int info, std::
         iSeason = item->GetPVRTimerInfoTag()->GetEpgInfoTag()->SeriesNumber();
       else if (item->HasVideoInfoTag())
         iSeason = item->GetVideoInfoTag()->m_iSeason;
+      else if (item->HasPVRRecordingInfoTag())
+        iSeason = item->GetPVRRecordingInfoTag()->m_iSeason;
 
       if (iSeason >= 0)
         return StringUtils::Format("%d", iSeason);
@@ -5407,6 +5416,8 @@ std::string CGUIInfoManager::GetItemLabel(const CFileItem *item, int info, std::
         return item->GetEPGInfoTag()->EpisodeName();
       if (item->HasPVRTimerInfoTag() && item->GetPVRTimerInfoTag()->HasEpgInfoTag())
         return item->GetPVRTimerInfoTag()->GetEpgInfoTag()->EpisodeName();
+      if (item->HasPVRRecordingInfoTag())
+        return item->GetPVRRecordingInfoTag()->EpisodeName();
       break;
     }
   case LISTITEM_TIMERTYPE:
