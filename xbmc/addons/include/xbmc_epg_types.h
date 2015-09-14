0ea680798742e235e0c9ca90e378ee002671c2d0
metaron <andrew@thomasinfletcher.com>
2015-09-14 17:39:56 +0100

    [PVR] API Add EPG Flags
    Add a bitfield (to be stored in EPG database) with room for future expansion

--- xbmc/addons/include/xbmc_epg_types.h
+++ xbmc/addons/include/xbmc_epg_types.h
@@ -61,6 +61,23 @@
 /* Set EPGTAG.iGenreType to EPG_GENRE_USE_STRING to transfer genre strings to XBMC */
 #define EPG_GENRE_USE_STRING                          0x100
 
+/* defines used by EPG_TAG.iFlags */
+#define EPG_TAG_FLAG_UNDEFINED                        0x00000000  /*!< @brief nothing special to say about this entry */
+#define EPG_TAG_FLAG_IS_SERIES                        0x00000001  /*!< @brief this EPG entry is part of a series */
+#define EPG_TAG_FLAG_IS_LIVE                          0x00000002  /*!< @brief this EPG entry is broadcast live */
+#define EPG_TAG_FLAG_IS_FINAL                         0x00000004  /*!< @brief this EPG entry is a competition final */
+#define EPG_TAG_FLAG_IS_MOVIE                         0x00000008  /*!< @brief this EPG entry is a movie (aka made for cinema / film) */
+#define EPG_TAG_FLAG_IS_NEW                           0x00000010  /*!< @brief this EPG entry is the first time this movie/episode/event has been shown on TV in this region (aka 'new', not a repeat, TV movie premiere) */
+#define EPG_TAG_FLAG_HAS_SUBTITLES                    0x00000020  /*!< @brief this EPG entry has subtitles */
+#define EPG_TAG_FLAG_HAS_SIGNING                      0x00000040  /*!< @brief this EPG entry has a sign language inset (for the deaf) */
+#define EPG_TAG_FLAG_HAS_AUDIO_DESCRIPTION            0x00000080  /*!< @brief this EPG entry has audio description (for partially sighted / blind) */
+#define EPG_TAG_FLAG_IS_WIDESCREEN                    0x00000100  /*!< @brief this EPG entry is shown in Widescreen (e.g. 16:9) format */
+#define EPG_TAG_FLAG_IS_HD                            0x00000200  /*!< @brief this EPG entry is shown in HD */
+#define EPG_TAG_FLAG_IS_3D                            0x00000400  /*!< @brief this EPG entry is shown in 3D */
+#define EPG_TAG_FLAG_IS_4K                            0x00000800  /*!< @brief this EPG entry is shown in 4k high resolution picture format */
+#define EPG_TAG_FLAG_HAS_SURROUND_SOUND               0x00001000  /*!< @brief this EPG entry has a surround sound audio track */
+#define EPG_TAG_FLAG_IS_SPLIT_EVENT                   0x00002000  /*!< @brief this EPG entry is part of a split event (e.g. part is shown before and after the news */
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -94,6 +111,7 @@ extern "C" {
     int           iEpisodeNumber;      /*!< @brief (optional) episode number */
     int           iEpisodePartNumber;  /*!< @brief (optional) episode part number */
     const char *  strEpisodeName;      /*!< @brief (optional) episode name */
+    unsigned int  iFlags;              /*!< @brief (optional) bit field of independent flags associated with the EPG entry, see #defines above */
   } ATTRIBUTE_PACKED EPG_TAG;
 
 #ifdef __cplusplus
